b87eeaf6526fc76293fc0264c5b14fe6a37a09d0
robahtou <61357564+robahtou@users.noreply.github.com>
2024-01-23 13:08:47 -0600

    use css to swap layouts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,44 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
 
-export default function Home() {
+const mainLayoutAdustment = {
+  'default-layout': 'primarysidebar-position-flipped',
+  'primarysidebar-position-flipped': 'default-layout'
+};
+const primarysidebarAdjustment = {
+  'primarysidebar-left': 'primarysidebar-right',
+  'primarysidebar-right': 'primarysidebar-left'
+};
+
+function changeLayout(e) {
+  const primarysidebarPosition = e.target.classList[1];
+  e.target.classList.replace(primarysidebarPosition, primarysidebarAdjustment[primarysidebarPosition]);
+
+  const wrapper = document.querySelector('.wrapper');
+  const mainLayout = wrapper?.classList[1];
+  wrapper?.classList.replace(mainLayout, mainLayoutAdustment[mainLayout]);
+}
+
+
+function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+      <div className="wrapper default-layout">
+        <header className="titlebar">
+          <div className="icon primarysidebar-left" onClick={(e) => changeLayout(e)}></div>
+        </header>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <section className="primarysidebar">
+          <aside className="activitybar"></aside>
+          <aside className="sidebar"></aside>
+        </section>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <section className="editor"></section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <footer className="statusbar"></footer>
       </div>
     </main>
   );
 }
+
+
+export default Home;
