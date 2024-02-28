5cf9bf563a738a739247595f51bf67871de7bd7e
robahtou <61357564+robahtou@users.noreply.github.com>
2024-02-28 13:11:11 -0600

    added secondary icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,22 @@
 import type { Metadata } from 'next';
-import './resets.css';
-import './global.css';
+
+import '../assets/styles/resets.css';
+// import '../assets/styles/global.css';
+import '../assets/styles/base.css';
+import '../assets/styles/utilities.css';
+import '../assets/styles/layouts.css';
+import '../assets/styles/dashboard.css';
 
 
 const metadata: Metadata = {
-  title: "Layouts with CSS Grids"
+  title: 'Layouts with CSS Grids'
 };
 
 function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body>
-          {children}
+        {children}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,86 @@
 'use client';
 
 
-const mainLayoutAdustment = {
-  'default-layout': 'primarysidebar-position-flipped',
-  'primarysidebar-position-flipped': 'default-layout'
+const mainLayout = {
+  'layout-default': 'layout-secondary',
+  'layout-secondary': 'layout-default'
 };
-const primarysidebarAdjustment = {
-  'primarysidebar-left': 'primarysidebar-right',
-  'primarysidebar-right': 'primarysidebar-left'
+const iconPrimarySwap = {
+  'sidepanel-left': 'sidepanel-right',
+  'sidepanel-right': 'sidepanel-left'
+};
+const iconSecondaryPositionSwap = {
+  'sidepanel-right': 'sidepanel-left',
+  'sidepanel-left': 'sidepanel-right',
+  'sidepanel-hidden-left': 'sidepanel-hidden-right',
+  'sidepanel-hidden-right': 'sidepanel-hidden-left'
+};
+const iconSecondaryVisibleSwap = {
+  'sidepanel-hidden-left': 'sidepanel-left',
+  'sidepanel-hidden-right': 'sidepanel-right',
+  'sidepanel-right': 'sidepanel-hidden-right',
+  'sidepanel-left': 'sidepanel-hidden-left'
 };
 
-function Home() {
-  const handleClick = e => {
-    const primarysidebarPosition = e.target.classList[1];
-    e.target.classList.replace(primarysidebarPosition, primarysidebarAdjustment[primarysidebarPosition]);
+function Page() {
+  const handlePrimaryPanelClick = e => {
+    const primaryIcon = e.target;
+    const secondaryIcon = e.target.nextElementSibling;
+    const main = document.getElementById('main');
 
-    const wrapper = document.querySelector('.wrapper');
-    const mainLayout = wrapper?.classList[1];
-    wrapper?.classList.replace(mainLayout, mainLayoutAdustment[mainLayout]);
-  }
+    const currentPrimaryIcon = primaryIcon.classList[1];
+    const currentSecondaryIcon = secondaryIcon.classList[1];
+    const currentLayout = main.classList[1];
+
+    primaryIcon.classList.replace(currentPrimaryIcon, iconPrimarySwap[currentPrimaryIcon]);
+    secondaryIcon.classList.replace(currentSecondaryIcon, iconSecondaryPositionSwap[currentSecondaryIcon]);
+    main.classList.replace(currentLayout, mainLayout[currentLayout]);
+  };
+
+  const handleSecondaryPanelClick = e => {
+    const secondaryIcon = e.target;
+    const secondarySidePanel = document.querySelector('.secondary-sidepanel');
+    const currentSecondaryIcon = secondaryIcon.classList[1];
+    const currentSecondarySidePanel = secondarySidePanel.classList[1];
+
+    secondaryIcon.classList.replace(currentSecondaryIcon, iconSecondaryVisibleSwap[currentSecondaryIcon]);
+
+    if (currentSecondarySidePanel === 'hidden') {
+      secondarySidePanel?.classList.remove('hidden');
+    } else {
+      secondarySidePanel?.classList.add('hidden');
+    }
+  };
 
   return (
-    <main>
-      <div className="wrapper default-layout">
-        <header className="titlebar">
-          <div className="icon primarysidebar-left" onClick={handleClick}></div>
-        </header>
+    <main id="main" className="grid layout-default">
+      <header className="titlebar">
+        <div className="icon sidepanel-left" onClick={handlePrimaryPanelClick}></div>
+        <div className="icon sidepanel-hidden-right" onClick={handleSecondaryPanelClick}></div>
+      </header>
 
-        <section className="primarysidebar">
-          <aside className="activitybar"></aside>
-          <aside className="sidebar"></aside>
-        </section>
+      <section className="activitybar"></section>
 
-        <section className="editor"></section>
+      <section className="primary-sidepanel">
+        <header className="somebar"></header>
+        <main className="some-panel"></main>
+      </section>
 
-        <footer className="statusbar"></footer>
-      </div>
+      <section className="control-panel">
+        <header className="controlbar"></header>
+        <div className="panel-one"></div>
+        <div className="panel-two"></div>
+      </section>
+
+      <section className="secondary-sidepanel hidden">
+        <header className="somebar"></header>
+        <main className="some-panel"></main>
+      </section>
+
+      <footer className="statusbar"></footer>
     </main>
   );
 }
 
 
-export default Home;
+export default Page;
